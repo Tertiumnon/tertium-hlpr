7b4b3d763140c48a988c5e5cfd13da08f4862d16
Vitalii Balabanov <tertiumnon@gmail.com>
2024-11-24 17:04:59 +0100

    Update init.sh

--- nix/ssh/init.sh
+++ nix/ssh/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 mkdir ~/.ssh
-touch ~/.ssh/authorized_keys
 touch ~/.ssh/known_hosts
+touch ~/.ssh/config
 chmod 700 ~/.ssh
-chmod 644 ~/.ssh/authorized_keys
 chmod 644 ~/.ssh/known_hosts
+chmod 644 ~/.ssh/config
